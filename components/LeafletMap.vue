891bc3b6e6ba0be6b742892e726abd907fe736a6
Sebastian Nyberg <sn@sentian.ai>
2018-05-09 12:18:00 +0200

    feat: initial commit with working map

---
+++ components/LeafletMap.vue
@@ -0,0 +1,31 @@
+<template>
+  <no-ssr>
+    <l-map class="mini-map" :zoom=13 :center="[47.413220, -1.219482]">
+      <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+      <l-marker :lat-lng="[47.413220, -1.219482]"></l-marker>
+    </l-map>
+  </no-ssr>
+</template>
+
+<script>
+// let Vue2Leaflet = {};
+// if (process.browser) {
+//   Vue2Leaflet = require("vue2-leaflet");
+// }
+// export default {
+//   components: {
+//     "v-map": Vue2Leaflet.Map,
+//     "v-tilelayer": Vue2Leaflet.TileLayer,
+//     "v-marker": Vue2Leaflet.Marker
+//   }
+// };
+</script>
+
+<style src="leaflet/dist/leaflet.css"></style>
+<style>
+.mni-map {
+  width: 100%;
+  height: 500px !important;
+}
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="container">
+    <leaflet-map id="leafletmap"></leaflet-map>
+  </section>
+</template>
+
+<script>
+import LeafletMap from "~/components/LeafletMap.vue";
+
+export default {
+  components: {
+    LeafletMap
+  }
+};
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+#leafletmap {
+  min-height: 100vh;
+  min-width: 100vh;
+}
+</style>
